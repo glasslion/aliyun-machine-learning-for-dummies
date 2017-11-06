c2048727393c0871bf9c49ee25a71cb1d34f26c8
Leo Zhou <glasslion@gmail.com>
2017-11-06 20:43:14 +0800

    support creating a snapshot before deleting a disk

--- delete_instance.py
+++ delete_instance.py
@@ -3,6 +3,9 @@ import time
 
 import click
 from aliyunsdkecs.request.v20140526 import (DeleteInstanceRequest,
+                                            DeleteSnapshotRequest,
+                                            DeleteDiskRequest,
+                                            CreateSnapshotRequest,
                                             StopInstanceRequest)
 from utils import Config, do_action, wait_for_instance_status
 
@@ -15,10 +18,15 @@ def main():
     stop_instance(config)
     wait_for_instance_status(config, "Stopped")
     delete_instance(config)
+    answer = click.prompt("是否要为当前的数据盘创建一个快照, 然后删除数据盘， 以节约费用? [y/n]")
+    if answer == 'y':
+        create_snapshot(config)
+        delete_disk(config)
     cleanup(config)
 
 
 def stop_instance(config):
+    click.echo(click.style("正在停止实例 ...", fg="green"))
     client = config.create_api_client()
     req = StopInstanceRequest.StopInstanceRequest()
     req.set_InstanceId(config.get('InstanceId'))
@@ -26,12 +34,38 @@ def stop_instance(config):
 
 
 def delete_instance(config):
+    click.echo(click.style("正在删除实例 ...", fg="green"))
     client = config.create_api_client()
     req = DeleteInstanceRequest.DeleteInstanceRequest()
     req.set_InstanceId(config.get('InstanceId'))
     result = do_action(client, req)
 
+def create_snapshot(config):
+    client = config.create_api_client()
+    OldSnapshotId = config.get('SnapshotId')
+    if OldSnapshotId:
+        request = DeleteSnapshotRequest.DeleteSnapshotRequest()
+        request.set_SnapshotId(OldSnapshotId)
+        do_action(client, request)
+
+    request = CreateSnapshotRequest.CreateSnapshotRequest()
+    request.set_DiskId(config.get('DiskId'))
+    result = do_action(client, request)
+    SnapshotId= result['SnapshotId']
+    config.set('SnapshotId', SnapshotId)
+
+def delete_disk(config):
+    client = config.create_api_client()
+    request = DeleteDiskRequest.DeleteDiskRequest()
+    request.set_DiskId(config.get('DiskId'))
+    do_action(client, request)
+    config.pop("DiskId")
+
+
+
+
 def cleanup(config):
+    click.echo(click.style("正在更新配置文件 ...", fg="green"))
     config.pop("InstanceId")
     config.save()
 
