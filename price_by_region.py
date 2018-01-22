6a02fafd6c32d424f9baa78c19be848444028467
Leo Zhou <glasslion@gmail.com>
2018-01-22 21:19:05 +0800

    speed up with thread pool

--- price_by_region.py
+++ price_by_region.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import concurrent.futures
+
 from tabulate import tabulate
 
 import click
@@ -10,38 +12,69 @@ from aliyunsdkecs.request.v20140526 import DescribeRegionsRequest
 from aliyunsdkecs.request.v20140526 import DescribeZonesRequest
 
 
+def query_price(config, region, zone, instance_type):
+    client  = config.create_api_client(region['RegionId'])
+    request = DescribeSpotPriceHistoryRequest.DescribeSpotPriceHistoryRequest()
+    request.set_ZoneId(zone['ZoneId'])
+    request.set_NetworkType('vpc')
+    request.set_InstanceType(instance_type)
+    result = do_action(client, request)
+    ret = []
+    try:
+        item = result['SpotPrices']['SpotPriceType'][-1]
+        ret.append(
+            [
+                zone['LocalName'],
+                instance_type,
+                item['Timestamp'],
+                item['SpotPrice']
+            ]
+        )
+    except IndexError:
+        pass
+    return ret
+
+
 @click.command()
-def main():
+@click.option('--max-price', type=int, default=1000)
+def main(max_price):
     config = Config()
     config.obtain_secret('access_key_id')
     config.obtain_secret('access_key_secret')
 
     click.echo("由于需要对地域，可用区， 实例类型的每种组合都查询一次价格， 查询时间可能较长， 请耐心等待")
-    table = []
+
     client  = config.create_api_client('cn-hongkong')
     regions = get_regions(client)
-    for region in regions:
-        client  = config.create_api_client(region['RegionId'])
-        zones = get_zones(client)
-        for zone in zones:
-            ins_types = zones[0]['AvailableInstanceTypes']['InstanceTypes']
-            ins_types = [t for t in ins_types if t.startswith('ecs.gn')]
-            if 'ecs.gn5-c4g1.xlarge' not in ins_types:
-                # 由于阿里云方面的bug, DescribeZonesRequest 返回的 AvailableInstanceTypes 可能不全
-                # 这里手动插入些最常用的实例类型
-                ins_types.append('ecs.gn5-c4g1.xlarge')
-            for instance_type in ins_types:
-                request = DescribeSpotPriceHistoryRequest.DescribeSpotPriceHistoryRequest()
-                request.set_ZoneId(zone['ZoneId'])
-                request.set_NetworkType('vpc')
+    futures = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        for region in regions:
+            client  = config.create_api_client(region['RegionId'])
+            zones = get_zones(client)
+            for zone in zones:
+                ins_types = zones[0]['AvailableInstanceTypes']['InstanceTypes']
+                ins_types = [t for t in ins_types if t.startswith('ecs.gn')]
+                if 'ecs.gn5-c4g1.xlarge' not in ins_types:
+                    # 由于阿里云方面的bug, DescribeZonesRequest 返回的 AvailableInstanceTypes 可能不全
+                    # 这里手动插入些最常用的实例类型
+                    ins_types.append('ecs.gn5-c4g1.xlarge')
+                for instance_type in ins_types:
+                    # query_price(config, region, zone, instance_type)
+                    future = executor.submit(
+                        query_price,
+                        config, region, zone, instance_type,
+                    )
 
-                request.set_InstanceType(instance_type)
-                result = do_action(client, request)
-                try:
-                    item = result['SpotPrices']['SpotPriceType'][-1]
-                    table.append([zone['LocalName'], instance_type, item['Timestamp'], item['SpotPrice']])
-                except IndexError:
-                    pass
+                    futures.append(future)
+    table = []
+    for future in concurrent.futures.as_completed(futures):
+        try:
+            data = future.result()
+            table.extend(data)
+        except Exception as exc:
+            print('Generated an exception: %s' % (exc))
+
+    table  = [row for row in table if row[-1]< max_price]
     table.sort(key=lambda x: (x[0], x[-1]))
     print(tabulate(table))
 
