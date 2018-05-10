5f1069f3dcb4ffa7c99824edf791a680a4c81751
Leo Zhou <glasslion@gmail.com>
2018-05-10 21:08:39 +0800

    disable https for jupyter notebook, it seems that https is forbidden in aliyun outside china

--- assets/jupyter_notebook_config.py
+++ assets/jupyter_notebook_config.py
@@ -1,6 +1,7 @@
 # Set options for certfile, ip, password, and toggle off browser auto-opening
-c.NotebookApp.certfile = u'/home/ml/.jupyter/mycert.pem'
-c.NotebookApp.keyfile = u'/home/ml/.jupyter/mykey.key'
+# c.NotebookApp.certfile = u'/home/ml/.jupyter/mycert.pem'
+# c.NotebookApp.keyfile = u'/home/ml/.jupyter/mykey.key'
+
 # Set ip to '*' to bind on all interfaces (ips) for the public server
 c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
