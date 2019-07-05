cc39529509b083cd37c6ff80f8d97c028406c5b2
Leo Zhou <glasslion@gmail.com>
2019-07-05 15:15:31 +0800

    colored select

--- utils/select.py
+++ utils/select.py
@@ -1,5 +1,8 @@
-import click
 import time
+import itertools
+
+import click
+import colorful
 
 from .action import do_action
 
@@ -21,20 +24,13 @@ class BaseConfigParameterSelect(object):
                 return param
 
         items = self.get_items()
-
         if len(items)==0:
             self.fix_empty_items()
             time.sleep(1)
             return self.show()
 
-        # format items
-        select_list = '\n'.join('[{}] - {}'.format(
-            idx, self.select_item_formatter(item)
-        ) for idx, item in enumerate(items))
-
-
-        msg = '可选的 {}:\n{}\n请选择实例的 {}（序号）'.format(self.name, select_list, self.name)
-        idx = click.prompt(msg, type=int)
+        formatted = self.format_items(items)
+        idx = click.prompt(formatted, type=int)
         param = items[idx][self.item_key]
         self.config.set(self.key, param)
         self.handle_selected_item(items[idx])
@@ -49,6 +45,9 @@ class BaseConfigParameterSelect(object):
         raise NotImplementedError('Empty Select List !!!')
 
     def get_items(self):
+        """
+        Get a list of items for select
+        """
         request = self.request_cls()
         self.set_request_parameters(request)
         result = do_action(self.client, request)
@@ -59,3 +58,23 @@ class BaseConfigParameterSelect(object):
             items.sort(key=lambda x: x[self.select_sorting])
 
         return items
+
+    def format_items(self,items):
+        lines = [
+            '[{}] - {}'.format(idx, self.select_item_formatter(item))
+             for idx, item in enumerate(items)
+        ]
+        select_list = '\n'.join(color_text(lines))
+        formatted = '可选的 {}:\n{}\n请选择实例的 {}（序号）'.format(
+            self.name, select_list, self.name)
+        return formatted
+
+
+def color_text(lines):
+    colors = [
+        colorful.yellow, colorful.orange, colorful.magenta,
+        colorful.green, colorful.blue, colorful.cyan,
+    ]
+
+    colored = [color(line) for line, color in zip(lines, itertools.cycle(colors))]
+    return [str(line) for line in colored]
